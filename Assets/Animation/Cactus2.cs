e07c37d7fbc2ed2ec78e3215b62a8d881dc7c407
Cezar Lupu <cezarlupu2012@gmail.com>
2021-10-07 22:10:15 +0300

    Tema1IMR

---
+++ Assets/Animation/Cactus2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cactus2 : MonoBehaviour
+{
+    public Transform cactus2;
+    private Animator anim2; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim2 = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, cactus2.position) < 5){
+            anim2.SetTrigger("animation");
+        }
+    }
+}
